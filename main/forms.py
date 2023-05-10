0ce0b8757af553c5cf132aee61e6da9b3398695a
Seva Arkhipenko <seva@Sevas-MacBook-Air.local>
2023-05-10 11:41:06 -0500

    removed files from the wrong directory

--- main/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class CreateNewWallet(forms.Form):
-    card_type = (("VISA","VISA"), ("MASTERCARD","MASTERCARD"))
-    currency_type = (("RUB", "RUB"), ("USD", "USD"), ("EUR", "EUR"))
-    type = forms.ChoiceField(required=True, choices=card_type)
-    currency = forms.ChoiceField(required=True, choices=currency_type)
